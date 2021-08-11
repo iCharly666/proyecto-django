f34665c6511a8393af5bc6be9ca633d9cd3dc0ba
iCharly666 <75290791+iCharly666@users.noreply.github.com>
2021-08-11 01:09:15 -0500

    Agregando proyecto prueba

---
+++ prueba/prueba.py
@@ -0,0 +1,47 @@
+"""x=5
+y=5
+print(x+y)
+
+
+"""
+
+'''SUMA '''
+No1=int(input('ingresa un numero: '))
+No2=int(input("ingresa otro numero: "))
+print('La suma total es ', (No1 + No2))
+
+""" DIVISION """
+No1=int(input("ingresa un numero: "))
+No2=int(input("ingresa otro numero: "))
+print('La division del No2/No1 es: ', (No2/No1))
+print('La parte entera de la division es: ', (No2//No1))
+print('El residuo de la division es: ', (No2%No1))
+
+""" NUMERO 2 ELEVADO AL NUMERO 1 """
+No1=int(input("ingresa un numero: "))
+No2=int(input("ingresa otro numero: "))
+print('Su numero elevado es: ', (No2**No1))
+
+
+# GASOLINA
+'''
+costo=float(input('Ingresa el costo/litro de gasolina: '))
+cantidad=int(input('Ingresa la cantidad a comprar: '))
+monto=costo*cantidad
+print('costo es:',costo,
+     '\ncandidad es:',cantidad, 
+	'\nmonto:',monto)
+
+monto=costo*cantidad
+if (float(monto)>=500 and float(monto)<1000):
+	des=monto*0.025
+	print('Monto del descuento', des)
+	total=monto-des
+	print('Total: ', total)
+elif (float(monto)>=1000):
+	des=monto*0.10
+	print('Monto del descuento', des)
+	total=monto-des
+	print('Total: ', total)
+'''
+
